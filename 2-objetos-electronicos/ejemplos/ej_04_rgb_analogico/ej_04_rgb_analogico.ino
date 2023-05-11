9157ae1ca326ec6046e6d71dcf9e5db2b2092248
sergiomoradiaz <smorad@gmail.com>
2023-05-10 20:12:33 -0400

    Update ej_04_rgb_analogico.ino

--- 2-objetos-electronicos/ejemplos/ej_04_rgb_analogico/ej_04_rgb_analogico.ino
+++ 2-objetos-electronicos/ejemplos/ej_04_rgb_analogico/ej_04_rgb_analogico.ino
@@ -1,9 +1,9 @@
 // ej_04_rgb_analogico
 // por sergiomoradiaz
 // para Academia Sinestesia
-// Programa de Medios Interactivos 2022
-// v0.0.1 agosto 2022
-// hecho con Arduino 1.8.19
+// Programa Medios Interactivos 2023
+// v0.0.2 mayo 2023
+// hecho con Arduino 2.1.0
 
 // declarar variables
 int pinSensor = A0; // sensor en pin A0
