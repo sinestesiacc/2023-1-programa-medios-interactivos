b0bde4bffb0e8ec4e3f440a6054e801507e17b07
sergiomoradiaz <smorad@gmail.com>
2023-05-10 20:12:18 -0400

    Update ej_03_rgb_digital.ino

--- 2-objetos-electronicos/ejemplos/ej_03_rgb_digital/ej_03_rgb_digital.ino
+++ 2-objetos-electronicos/ejemplos/ej_03_rgb_digital/ej_03_rgb_digital.ino
@@ -1,9 +1,9 @@
 // ej_03_rgb_digital
 // por sergiomoradiaz
 // para Academia Sinestesia
-// Programa de Medios Interactivos 2022
-// v0.0.1 agosto 2022
-// hecho con Arduino 1.8.19
+// Programa Medios Interactivos 2023
+// v0.0.2 mayo 2023
+// hecho con Arduino 2.1.0
 
 // declarar variables
 int pinSensor = 12; // sensor en pin 12
